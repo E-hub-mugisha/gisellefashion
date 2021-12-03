2acb4544a8839f71911380d9bbbc283caac4c32b
mugisha erik <kabosierik@gmail.com>
2021-12-03 09:23:13 +0200

    cancel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import Header from './lambda/Header';
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
-function App(){
-  return (
-    <>
-    <Header />
-    </>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
