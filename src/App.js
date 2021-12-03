de444a419a1df1737b358206d93712e2372c124a
mugisha erik <kabosierik@gmail.com>
2021-12-03 09:30:07 +0200

    chane

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
-
+import Header from './lambda/Header';
 
 class App extends Component {
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
           </p>
           
         </header>
+        <Header />
       </div>
     )
   }
