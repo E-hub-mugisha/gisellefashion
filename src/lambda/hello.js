be21683d52c2a95a422cfbfb81d35da2e7c5fa68
mugisha erik <kabosierik@gmail.com>
2021-12-03 08:37:01 +0200

    first change

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,8 +1,1 @@
-// this uses the callback syntax, however, we encourage you to try the async/await syntax shown in async-dadjoke.js
-export function handler(event, context, callback) {
-  console.log('queryStringParameters', event.queryStringParameters)
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({ msg: 'Hello, World!' }),
-  })
-}
+<h2>hello giselle</h2>
